5e64969e80a32dc104b2f0790d4fa4a4c13fbf18
Mohanpoluri <polurimohan1@gmail.com>
2025-08-24 11:24:01 +0530

    Initial commit - Simple React Calculator

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState("");
+
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+
+  const handleClear = () => {
+    setInput("");
+  };
+
+  const handleCalculate = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch {
+      setInput("Not a valid expression");
+    }
+  };
+
+  return (
+    <div className="app">
+      <div className="calculator">
+        <h2>Mohan's 🧮 React Calculator</h2>
+        <input type="text" value={input} readOnly />
+        <div className="buttons">
+          <button onClick={() => handleClick("7")}>7</button>
+          <button onClick={() => handleClick("8")}>8</button>
+          <button onClick={() => handleClick("9")}>9</button>
+          <button onClick={() => handleClick("/")}>/</button>
+          <button onClick={() => handleClick("4")}>4</button>
+          <button onClick={() => handleClick("5")}>5</button>
+          <button onClick={() => handleClick("6")}>6</button>
+          <button onClick={() => handleClick("*")}>*</button>
+          <button onClick={() => handleClick("1")}>1</button>
+          <button onClick={() => handleClick("2")}>2</button>
+          <button onClick={() => handleClick("3")}>3</button>
+          <button onClick={() => handleClick("-")}>-</button>
+          <button onClick={() => handleClick("0")}>0</button>
+          <button onClick={() => handleClick(".")}>.</button>
+          <button onClick={() => handleClick("+")}>+</button>
+          <button className="clear" onClick={handleClear}>C</button>
+          <button style={{ width: "120px"}} className="equal" onClick={handleCalculate}>=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
