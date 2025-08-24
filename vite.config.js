ab2731f0d734f59ace071ad8679a27e37748fd1d
Mohanpoluri <polurimohan1@gmail.com>
2025-08-24 11:34:36 +0530

    Initial commit - Simple React Calculator

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/Simple-react-calculator/',   
-=======
-  base: '/Simple-react-calculator/',
->>>>>>> 51003b63fd5bbf34b4434d5e2384931198016cae
+  base: '/Simple-react-calculator/', // 👈 must match GitHub repo name
 })
